518483b1c006fe0e4d25dd0b0f9a3491135e3e71
Frozenmix <westernplague@mail.ru>
2020-12-11 21:38:16 +0700

    First Home Task

---
+++ HomeTask1/HomeTask1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HomeTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}! Today is [{DateTime.Now.ToString("dd.MM.yyyy")}]");
+        }
+    }
+}
