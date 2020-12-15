f7bc08a5c5b2203792b42384128114e48ec7061a
Frozenmix <westernplague@mail.ru>
2020-12-15 21:59:57 +0700

    Part 1 of Task 2

---
+++ Hometask2/Hometask2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hometask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите минимальную температуру за сутки");
+            var lowestTemp = Console.ReadLine();
+            Console.WriteLine("Введите максимальную температуру за сутки");
+            var highestTemp = Console.ReadLine();
+            int a = Convert.ToInt32(highestTemp);
+            int b = Convert.ToInt32(lowestTemp);
+            if (b > a) {
+                Console.WriteLine("Минимальная температура НЕ может быть выше максимальной");
+                return;
+            }         
+            double averageTemp = (a + b)/2;            
+            Console.WriteLine("Средняя температура за сутки "  +  averageTemp);
+            return;
+        }
+    }
+}
