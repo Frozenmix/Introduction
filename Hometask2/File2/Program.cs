dbfb86c7aab9266acc043ba21c35756aa4b76d8a
Frozenmix <westernplague@mail.ru>
2020-12-15 22:31:02 +0700

    Done Part 2

---
+++ Hometask2/File2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File2
+{
+    class Program
+    { 
+        enum Month {
+            January = 1,
+            February,
+            March,
+            April,
+            May,
+            June,
+            July,
+            August,
+            September,
+            October,
+            November,
+            December,
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the current month's number");
+            var Aquirednumber = Console.ReadLine();
+            int a = Convert.ToInt32(Aquirednumber);
+            Month month = (Month)a;
+            Console.WriteLine("It's " + month);    
+        }
+    }
+}
